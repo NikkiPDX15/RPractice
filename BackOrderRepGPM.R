6715901451d3db428f6dff36d1b1365eb2377454
Nikki Freeman <nfreeman2015@gmail.com>
2017-07-09 19:30:01 -0700

    Update BackOrderRepGPM.R

--- BackOrderRepGPM.R
+++ BackOrderRepGPM.R
@@ -6,43 +6,54 @@
 # Purpose of this program is to read, sort, and compare the 
 # Master Back order sheet as well as the GPM Container Log
 
+# This compare report takes 15-20 minutes to do manually
+# Now it only takes 2 minutes
+# 1.75 minutes would be self formatting and printing the sheet
+
+# Back Order CSV sheet is imported
 BackOrder <- read.table(file.choose(),na.strings=c("", "NA"), header=T, sep=",")
-
-BOList <- as.list(BackOrder$CONTAINER.TRLR)
-
+# Subset dataframe to choose only GPMs Back Orders
 sub1 <- subset(BackOrder,BackOrder$WHS ==440)
+# Subset to keep columns we need
 sub2 <- sub1[c(2,3,8,12:18)]
 
-#IS.NA.useful some other time 
-#BORep <-sub2[rowSums(is.na(sub2[8,])) == 0, ]
-#sub3 <- as.list(sub2[8])
-
-#unique(sub3)
-#News <- subset(sub3, !duplicated(BackOrder$CONTAINER.TRLR))
-
-
+# Container CSV sheet is imported
 ConLog <- read.table(file.choose(),na.strings=c("", "NA"), header=T, sep=",")
+# Subset what columns we need
 ConDoor <- ConLog[c(3:5)]
+# get rid of rows that do not have a container under container column
+# this also means that we will not be able to see the UPS with this comparison
+# which is why an Overall Back Order sheet is printed :)
 NADoor <- as.array(which(!is.na(ConDoor$CONTAINER)))
 ConDoor2 <- data.frame(ConDoor[NADoor,])
 
-
-Print1 <- as.array(which(sub2$CONTAINER.TRLR %in% ConDoor$CONTAINER)) # Want to keep UPS
+# compares the two sheets, generates which rows have a container value shared
+# in both the Back Order sheet and the Container Log
+Print1 <- as.array(which(sub2$CONTAINER.TRLR %in% ConDoor$CONTAINER)) 
 Print2 <- as.array(which(ConDoor2$CONTAINER %in% sub2$CONTAINER.TRLR)) 
 
+# creates final data frame 
 FinalBOReport <- data.frame(sub2[Print1, 1:10]) # want to Keep UPS 
 FinalCD <- data.frame(ConDoor2[Print2, 1:3])
-
+#Merges data to one clean sheet
 Ultimate <- merge.data.frame(FinalCD, FinalBOReport, 
                              by.x = "CONTAINER", 
                              by.y = "CONTAINER.TRLR", all.x = TRUE)
 
 # Below Prints the Containers that are in the Yard that 
-# have backorder parts on them that need to be emptied still
+# have backorder parts on them that still need to be emptied
 
 write.table(Ultimate, file = "BackOrderGPM_InYard.csv",row.names=FALSE, na="",
             col.names=TRUE, sep=",")  
 
 # Below is the overall backorder report that includes UPS.
+
 write.table(sub2, file = "BackOrderGPM_OverAll.csv",row.names=FALSE, na="",
           col.names=TRUE, sep=",")
+
+
+# For New computers using this program:
+#set work directory so files can be saved in the area you want it to be saved
+# First, find path then you can set it in the folder you want
+# getwd()
+# setwd("")
