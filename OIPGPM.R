39dc7b01e4b103a32980a4d9b89aa26bcc091ddd
Nikki Freeman <nfreeman2015@gmail.com>
2017-07-20 20:06:46 -0700

    Add files via upload

--- OIPGPM.R
+++ OIPGPM.R
@@ -4,9 +4,40 @@
 # Started Date: 7.12.2017
 
 # install.packages("gdata")  for reading excel future use**
+# install.packages("lubridate")
 
 #Ask today's date for late comparison
 Date <- readline(prompt = "What is today's date (M/DD/2017): ")
+
+require(lubridate)
+WeekOut1 <- as.Date(Date, format='%m/%d/%Y') - 1
+WeekOut2 <- as.Date(WeekOut1, format='%m/%d/%Y') - 1
+WeekOut3 <- as.Date(WeekOut2, format='%m/%d/%Y') - 1
+WeekOut4 <- as.Date(WeekOut3, format='%m/%d/%Y') - 1
+WeekOut5 <- as.Date(WeekOut4, format='%m/%d/%Y') - 1
+WeekOut6 <- as.Date(WeekOut5, format='%m/%d/%Y') - 1
+
+
+SNESDC1<- format(WeekOut1, format='%m/%d/%Y')
+SNESDC2<- format(WeekOut2, format='%m/%d/%Y')
+SNESDC3<- format(WeekOut3, format='%m/%d/%Y')
+SNESDC4<- format(WeekOut4, format='%m/%d/%Y')
+SNESDC5<- format(WeekOut5, format='%m/%d/%Y')
+SNESDC6<- format(WeekOut6, format='%m/%d/%Y')
+
+SDCSNE_OKDate <- c(Date,SNESDC1, SNESDC2, SNESDC3, SNESDC4, SNESDC5, SNESDC6)
+
+
+# Date reviews
+# http://www.statmethods.net/input/dates.html
+
+
+
+# save for todays date
+# today <- Sys.Date()
+# format(today, format="%m/%d/%y")
+
+
 #Read in csv from Red Prairie
 df1 = read.csv (file.choose(), na.strings=c("", "NA"))
 # Rid of blank rows
@@ -18,9 +49,12 @@ col_headings <- c('Order#','Dealer','Order', 'Stat', 'Lines',
                   'SDN#', 'Weight', 'Cube', 'Stock Days', 
                   'Oracle Date', 'Red Prairie Date')
 names(df3) <- col_headings
+OrderDone= c("INTE", "INDE")
+
+df33<- subset(df3, df3$Order == OrderDone)
 
 #gets rid of NA's in rows
-df4 <- df3[which(!is.na(df3$`Order#`)),]
+df4 <- df33[which(!is.na(df33$`Order#`)),]
 #Sets late bar
 Late <- subset(df4, df4$`Oracle Date` != Date)
 
@@ -207,6 +241,59 @@ ResultPLO_LATE_PendLoad = (sum(ArrayPLO_PENDING_LOAD_Total) - sum(ArrayPLO_OK_PE
 ResultPLO_OKPendLoad = sum(ArrayPLO_OK_PENDING_LOAD)
 
 
+############################
+########### SDC ###########
+
+
+SDC <- df4[df4$`Stock Days` %in% "SDC",]
+
+SDC_IP_Total <- SDC[SDC$Stat %in% InProcess,]
+SDC_IP_OK <- subset(SDC_IP_Total, SDC_IP_Total$`Oracle Date` == SDCSNE_OKDate)
+SDC_PENDING_LOAD_Total <- SDC[SDC$Stat %in% PendingLoad,]
+SDC_OK_PENDING_LOAD <- subset(SDC_PENDING_LOAD_Total, SDC_PENDING_LOAD_Total$`Oracle Date` == SDCSNE_OKDate)
+
+#trun Lines column into array of numbers to total lines
+ArraySDC = as.numeric(as.array(SDC$Lines))
+Total_SDC_Lines <- sum(ArraySDC) # Total Lines for this RDC
+
+ArraySDC_IP_Total = as.numeric(as.array(SDC_IP_Total$Lines))
+ArraySDC_IP_OK = as.numeric(as.array(SDC_IP_OK$Lines))
+ArraySDC_PENDING_LOAD_Total = as.numeric(as.array(SDC_PENDING_LOAD_Total$Lines))
+ArraySDC_OK_PENDING_LOAD = as.numeric(as.array(SDC_OK_PENDING_LOAD$Lines))
+
+# Sum lines for RDC for Final break Down
+ResultSDC_LATEIP = (sum(ArraySDC_IP_Total) - sum(ArraySDC_IP_OK))
+ResultSDC_OKIP = sum(ArraySDC_IP_OK)
+ResultSDC_LATE_PendLoad = (sum(ArraySDC_PENDING_LOAD_Total) - sum(ArraySDC_OK_PENDING_LOAD))
+ResultSDC_OKPendLoad = sum(ArraySDC_OK_PENDING_LOAD)
+
+##############################
+#######SNE ##################
+
+SNE <- df4[df4$`Stock Days` %in% "SNE",]
+
+SNE_IP_Total <- SNE[SNE$Stat %in% InProcess,]
+SNE_IP_OK <- subset(SNE_IP_Total, SNE_IP_Total$`Oracle Date` == SDCSNE_OKDate)
+SNE_PENDING_LOAD_Total <- SNE[SNE$Stat %in% PendingLoad,]
+SNE_OK_PENDING_LOAD <- subset(SNE_PENDING_LOAD_Total, SNE_PENDING_LOAD_Total$`Oracle Date` == SDCSNE_OKDate)
+
+#trun Lines column into array of numbers to total lines
+ArraySNE = as.numeric(as.array(SNE$Lines))
+Total_SNE_Lines <- sum(ArraySNE) # Total Lines for this RDC
+
+ArraySNE_IP_Total = as.numeric(as.array(SNE_IP_Total$Lines))
+ArraySNE_IP_OK = as.numeric(as.array(SNE_IP_OK$Lines))
+ArraySNE_PENDING_LOAD_Total = as.numeric(as.array(SNE_PENDING_LOAD_Total$Lines))
+ArraySNE_OK_PENDING_LOAD = as.numeric(as.array(SNE_OK_PENDING_LOAD$Lines))
+
+# Sum lines for RDC for Final break Down
+ResultSNE_LATEIP = (sum(ArraySNE_IP_Total) - sum(ArraySNE_IP_OK))
+ResultSNE_OKIP = sum(ArraySNE_IP_OK)
+ResultSNE_LATE_PendLoad = (sum(ArraySNE_PENDING_LOAD_Total) - sum(ArraySNE_OK_PENDING_LOAD))
+ResultSNE_OKPendLoad = sum(ArraySNE_OK_PENDING_LOAD)
+
+
+
 
 ##################### Now we create a table for all of our Data################
 
@@ -259,7 +346,85 @@ OIP[7,1] = "PLO"
   OIP[7,5] = ResultPLO_LATE_PendLoad
   OIP[7,6] = ResultPLO_OKPendLoad
 OIP[8,1] = "SDC"
+  OIP[8,2] = Total_SDC_Lines
+  OIP[8,3] = ResultSDC_LATEIP
+  OIP[8,4] = ResultSDC_OKIP
+  OIP[8,5] = ResultSDC_LATE_PendLoad
+  OIP[8,6] = ResultSDC_OKPendLoad
 OIP[9,1] = "SNE"
+  OIP[9,2] = Total_SNE_Lines
+  OIP[9,3] = ResultSNE_LATEIP
+  OIP[9,4] = ResultSNE_OKIP
+  OIP[9,5] = ResultSNE_LATE_PendLoad
+  OIP[9,6] = ResultSNE_OKPendLoad
 
+OIP[10,1] = "Total"
+  OIP[10,2] = sum(Total_AUR_Lines,Total_COP_Lines,Total_DOU_Lines,Total_FLO_Lines,
+                  Total_LEB_Lines,Total_ONT_Lines, Total_PLO_Lines,
+                  Total_SDC_Lines,Total_SNE_Lines)
+  OIP[10,3] = sum(ResultAUR_LATEIP, ResultCOP_LATEIP, ResultDOU_LATEIP, ResultFLO_LATEIP,
+                  ResultLEB_LATEIP, ResultONT_LATEIP, ResultPLO_LATEIP,
+                  ResultSDC_LATEIP, ResultSNE_LATEIP)
+  OIP[10,4] = sum(ResultAUR_OKIP, ResultCOP_OKIP, ResultDOU_OKIP, ResultFLO_OKIP,
+                  ResultLEB_OKIP, ResultONT_OKIP, ResultPLO_LATEIP,
+                  ResultSDC_OKIP, ResultSNE_OKIP)
+  OIP[10,5] = sum(ResultAUR_LATE_PendLoad, ResultCOP_LATE_PendLoad, ResultDOU_LATE_PendLoad, 
+                  ResultFLO_LATE_PendLoad,ResultLEB_LATE_PendLoad, ResultONT_LATE_PendLoad,
+                  ResultPLO_LATE_PendLoad, ResultSDC_LATE_PendLoad, ResultSNE_LATE_PendLoad)
+  OIP[10,6] = sum(ResultAUR_OKPendLoad, ResultCOP_OKPendLoad, ResultDOU_OKPendLoad, 
+                  ResultFLO_OKPendLoad,ResultLEB_OKPendLoad, ResultONT_OKPendLoad,
+                  ResultPLO_OKPendLoad, ResultSDC_OKPendLoad, ResultSNE_OKPendLoad)
+
+  
+  
+#########################################
+  ########### RP PROCESS KEY #############
+  
+TenS <- df4[which(df4$Stat==10),]
+TenA <- as.array(TenS$Lines)
+Ten <- sum(as.numeric(TenA))
+
+EightyOneS <- df4[which(df4$Stat== 81),]
+EightyOneA <- as.array(EightyOneS$Lines)
+EightyOne <- sum(as.numeric(EightyOneA))
+
+EightyS <- df4[which(df4$Stat== 80),]
+EightyA <- as.array(EightyS$Lines)
+Eighty <- sum(as.numeric(EightyA))
+
+ZeroS <- df4[which(df4$Stat==0 & df4$Order == "INTE"),]
+ZeroA <- as.array(ZeroS$Lines)
+Zero <- sum(as.numeric(ZeroA))
+
+  
 write.table(OIP, file = "GPM_OIP.csv",row.names=FALSE, na="",
             col.names=TRUE, sep=",")  
+
+
+RPSTAT <- data.frame(matrix(ncol = 4, nrow = 4))
+x <- c("RP", "STATUS KEY", "STATUS", "Balances")
+colnames(RPSTAT) <- x
+
+RPSTAT[1,1] <- 10
+  RPSTAT[1,2] <- "PICKED/PACKED & PENDING LOAD"
+  RPSTAT[1,3]<- Ten
+
+RPSTAT[2,1] <- 81
+  RPSTAT[2,2] <- "PICKING IN-PROCESS"
+  RPSTAT[2,3] <- EightyOne
+  
+RPSTAT[3,1] <- 80
+  RPSTAT[3,2] <- "ALLOCATED WAITING TO BEGIN PICK"
+  RPSTAT[3,3] <- Eighty
+  
+RPSTAT[4,1] <- 0 
+  RPSTAT[4,2] <- "PARTIALLY ALLOCATED"
+  RPSTAT[4,3]<- Zero
+
+
+
+
+
+
+
+
